bab233891bd3f0644bdc86bf54d0b72ef80de3dd
Anshul Yadav <156102877+Anshu10101@users.noreply.github.com>
2024-08-28 14:04:02 +0530

    Time: 17 ms (14.12%), Space: 49.6 MB (77.14%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,6 +1,20 @@
 public class Solution {
-    public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        return nums[nums.length / 2];
-    }   
+   public int majorityElement(int[] nums) {
+        HashMap<Integer, Integer> countMap = new HashMap<>();
+        int majorityCount = nums.length / 2;
+
+        // Iterate through the array and count frequencies
+        for (int num : nums) {
+            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+
+            // Check if this element is the majority element
+            if (countMap.get(num) > majorityCount) {
+                return num;
+            }
+        }
+
+        // The problem guarantees that there will always be a majority element,
+        // so this return statement should never be reached.
+        return -1; 
+    } 
 }
