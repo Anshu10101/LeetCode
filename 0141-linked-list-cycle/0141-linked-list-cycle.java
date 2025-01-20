deed53c06192c889fc11865ec519545885e531bf
Anshul Yadav <156102877+Anshu10101@users.noreply.github.com>
2025-01-20 16:49:14 +0530

    0141-LINKED-LIST-CYCLE Stats: Time: 0 ms (100%), Space: 44.4 MB (60.8%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,13 +11,13 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        Set<ListNode> set = new HashSet<>();
-        while (head != null) {
-            if (set.contains(head)) {
+        ListNode slow_pointer = head, fast_pointer = head;
+        while (fast_pointer != null && fast_pointer.next != null) {
+            slow_pointer = slow_pointer.next;
+            fast_pointer = fast_pointer.next.next;
+            if (slow_pointer == fast_pointer) {
                 return true;
             }
-            set.add(head);
-            head = head.next;
         }
         return false;
     }
