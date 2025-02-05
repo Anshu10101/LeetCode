162349278545ae42c127945afa10de8a83132ae2
Anshul Yadav <156102877+Anshu10101@users.noreply.github.com>
2025-02-06 01:35:29 +0530

    1915-CHECK-IF-ONE-STRING-SWAP-CAN-MAKE-STRINGS-EQUAL Stats: Time: 1 ms (52.35%), Space: 41 MB (99.88%) - LeetHub

--- 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.java
+++ 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.java
@@ -6,19 +6,17 @@ class Solution {
         for (int k = 0; k < s1.length(); k++) {
             if (s1.charAt(k) != s2.charAt(k)) {
                 cnt++;
-                if (i == -1)
+                if (i == -1) // swapping the characters if counter increases
                     i = k;
                 else if (j == -1)
                     j = k;
             }
         }
-
         if (cnt == 0)
             return true;
         else if (cnt == 2 && s1.charAt(i) == s2.charAt(j) && s1.charAt(j) == s2.charAt(i)) {
-            return true;
-        }
-
+            return true; //after swapping when counter increased by 1
+        } 
         return false;
     }
 }
