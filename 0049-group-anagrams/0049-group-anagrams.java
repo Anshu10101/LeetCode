5734de9fb29befbc0159e39bcd2b05fc6d4d56a3
Anshul Yadav <156102877+Anshu10101@users.noreply.github.com>
2025-02-12 01:03:00 +0530

    0049-GROUP-ANAGRAMS Stats: Time: 7 ms (65.28%), Space: 47.8 MB (68.56%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -4,11 +4,11 @@ class Solution {
 
         for (String word : strs) {
             char[] chars = word.toCharArray();
-            Arrays.sort(chars);
-            String sortedWord = new String(chars);
+            Arrays.sort(chars); 
+            String sortedWord = new String(chars); // storing the sorted words as key in mp
 
-            if (!map.containsKey(sortedWord)) {
-                map.put(sortedWord, new ArrayList<>());
+            if (!map.containsKey(sortedWord)) { //verifying if it's in the mp or not
+                map.put(sortedWord, new ArrayList<>()); // putting the word in arraylist
             }
 
             map.get(sortedWord).add(word);
