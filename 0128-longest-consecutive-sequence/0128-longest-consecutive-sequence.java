56450067ae24c22d5ed0ea22c21ddd5b7bd9b73d
Anshul Yadav <156102877+Anshu10101@users.noreply.github.com>
2025-02-21 13:49:28 +0530

    0128-LONGEST-CONSECUTIVE-SEQUENCE Stats: Time: 33 ms (36.74%), Space: 67.9 MB (24.3%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -7,9 +7,11 @@ public class Solution {
         int longest = 0;
 
         for (int num : numSet) {
-            if (!numSet.contains(num - 1)) {
-                int length = 1;
-                while (numSet.contains(num + length)) {
+            if (!numSet.contains(num - 1)) {// check if there is start of a sequence and if not
+
+                int length = 1; // update the length=1; agar new sequence start hua
+                while (numSet.contains(num + length)) {// if hashset contains adjacent number 
+                //i.e. num+length(as adding length will ensure we get the adjacent element)
                     length++;
                 }
                 longest = Math.max(longest, length);
