84c3ab909d9ad1f1925ed7c1eef3f528beb04dd3
Anshul Yadav <156102877+Anshu10101@users.noreply.github.com>
2025-03-20 13:01:02 +0530

    0016-3SUM-CLOSEST Stats: Time: 14 ms (72.52%), Space: 43.9 MB (5.61%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -1,33 +1,33 @@
 class Solution {
-public int threeSumClosest(int[] nums, int target) {
-Arrays.sort(nums);
-int closestSum = nums[0] + nums[1] + nums[2];
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int closestSum = nums[0] + nums[1] + nums[2];
 
-    for (int i = 0; i < nums.length - 2; i++) {
-        int j = i + 1;
-        int k = nums.length - 1;
+        for (int i = 0; i < nums.length - 2; i++) {
+            int j = i + 1;
+            int k = nums.length - 1;
 
-        while (j < k) {
-            int sum = nums[i] + nums[j] + nums[k];
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
 
-            if (sum == target) {
-                return target; // If the sum is equal to the target, return immediately (closest possible sum)
-            }
+                if (sum == target) {
+                    return target; // If the sum is equal to the target, return immediately (closest possible sum)
+                }
 
-            // Update closestSum only when the current sum is closer to the target
-            if (Math.abs(target - sum) < Math.abs(target - closestSum)) {
-                closestSum = sum;
-            }
+                // Update closestSum only when the current sum is closer to the target
+                if (Math.abs(target - sum) < Math.abs(target - closestSum)) {
+                    closestSum = sum;
+                }
 
-            // Move pointers based on the comparison with the target
-            if (sum < target) {
-                j++;
-            } else {
-                k--;
+                // Move pointers based on the comparison with the target
+                if (sum < target) {
+                    j++;
+                } else {
+                    k--;
+                }
             }
         }
-    }
 
-    return closestSum;
-}
+        return closestSum;
+    }
 }
