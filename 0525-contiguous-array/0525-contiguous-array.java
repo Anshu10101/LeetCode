2a47fbb97aa01a07c4a31367bf2eaad65c54b864
Anshul Yadav <156102877+Anshu10101@users.noreply.github.com>
2025-06-06 10:51:32 +0530

    0525-CONTIGUOUS-ARRAY Stats: Time: 24 ms (54.22%), Space: 55.5 MB (40.26%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.java
+++ 0525-contiguous-array/0525-contiguous-array.java
@@ -1,26 +1,28 @@
 class Solution {
     public int findMaxLength(int[] nums) {
-        if (nums == null || nums.length == 0) { // Base Case
+        if (nums == null || nums.length == 0) { 
             return 0;
         }
-        // Converting all 0 to -1
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] == 0) nums[i] = -1;
-        }
-        int sum = 0; // current
-        int max = 0; // final-ans
-        Map<Integer, Integer> map = new HashMap<>();
-        map.put(0, -1); // put reference in the starting of 0 & -1, as i have tell you in the starting  
-        for(int i = 0; i < nums.length; i++){
-            sum += nums[i]; // cumulative sum
-            if(map.containsKey(sum)){ // if cumulative sum key :- 0, -1, 1 already present
-                int last = map.get(sum); // we get it's value
-                max = Math.max(max, i - last); // and update max
+        int ans = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        int sum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                sum += -1;
+            } else {
+                sum += 1;
             }
-            else{ // if it's not present then create it's key-value pair
+
+            if (map.containsKey(sum)) {
+                int idx = map.get(sum);
+                ans = Math.max(ans, i - idx);
+            } else {
                 map.put(sum, i);
             }
         }
-        return max; // finally return it
+
+        return ans;
     }
 }
