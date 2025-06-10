54fbf64f563a80563fc9920db8718dc512fb6f22
Anshul Yadav <156102877+Anshu10101@users.noreply.github.com>
2025-06-10 11:24:32 +0530

    0554-BRICK-WALL Stats: Time: 11 ms (96.65%), Space: 49.2 MB (23.79%) - LeetHub

--- 0554-brick-wall/0554-brick-wall.java
+++ 0554-brick-wall/0554-brick-wall.java
@@ -1,21 +1,25 @@
+
 class Solution {
     public int leastBricks(List<List<Integer>> wall) {
         Map<Integer, Integer> countGap = new HashMap<>();
 
         for (List<Integer> row : wall) {
             int total = 0;
-            // Skip the last brick to avoid the edge of the wall
+
             for (int i = 0; i < row.size() - 1; i++) {
                 total += row.get(i);
+
                 countGap.put(total, countGap.getOrDefault(total, 0) + 1);
             }
         }
 
         int maxGaps = 0;
-        for (int gapCount : countGap.values()) {
-            maxGaps = Math.max(maxGaps, gapCount);
+
+        for (int gapCounts : countGap.values()) {
+            maxGaps = Math.max(maxGaps, gapCounts);
         }
 
         return wall.size() - maxGaps;
+
     }
 }
