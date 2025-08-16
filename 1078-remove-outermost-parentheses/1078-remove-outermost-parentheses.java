1add27e509ce0258231f9db4d441e031ccfafbca
Anshul Yadav <156102877+Anshu10101@users.noreply.github.com>
2025-08-16 10:40:17 +0530

    1078-REMOVE-OUTERMOST-PARENTHESES Stats: Time: 2 ms (99.03%), Space: 42.3 MB (65.43%) - LeetHub

--- 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.java
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.java
@@ -1,14 +1,13 @@
 class Solution {
     public String removeOuterParentheses(String s) {
-        int count = 0; // To track the depth of parentheses
-        StringBuilder ans = new StringBuilder(); // To store the result
+        StringBuilder result = new StringBuilder();
+        int opened = 0;
 
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == ')') count--; // Decrease count when closing parenthesis is encountered
-            if (count != 0) ans.append(s.charAt(i)); // Append to result if count is not zero
-            if (s.charAt(i) == '(') count++; // Increase count when opening parenthesis is encountered
+        for(char c : s.toCharArray()) {
+            if((c == '(' && opened++ > 0) || (c == ')' && opened -- > 1)) {
+                result.append(c);
+            }
         }
-
-        return ans.toString(); // Convert StringBuilder to String and return
+        return result.toString();
     }
 }
