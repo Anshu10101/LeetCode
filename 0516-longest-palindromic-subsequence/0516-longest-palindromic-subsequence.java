b40d11ce56a0516b2133dd3c7fe898621f5127ab
Anshul Yadav <156102877+Anshu10101@users.noreply.github.com>
2025-09-02 10:03:44 +0530

    0516-LONGEST-PALINDROMIC-SUBSEQUENCE Stats: Time: 58 ms (32.49%), Space: 55.6 MB (20.95%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -1,17 +1,27 @@
-public class Solution {
-    public int longestPalindromeSubseq(String s) {
-        int[][] dp = new int[s.length()][s.length()];
-        
-        for (int i = s.length() - 1; i >= 0; i--) {
-            dp[i][i] = 1;
-            for (int j = i+1; j < s.length(); j++) {
-                if (s.charAt(i) == s.charAt(j)) {
-                    dp[i][j] = dp[i+1][j-1] + 2;
+class Solution {
+    // Function to calculate LCS
+    private int LCS(String a, String b, int m, int n) {
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (a.charAt(i - 1) == b.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
                 } else {
-                    dp[i][j] = Math.max(dp[i+1][j], dp[i][j-1]);
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
-        return dp[0][s.length()-1];
+
+        return dp[m][n];
+    }
+
+    // Function to compute Longest Palindromic Subsequence
+    public int longestPalindromeSubseq(String s) {
+        StringBuilder sb = new StringBuilder(s);
+        sb.reverse();
+        String rev = sb.toString();
+
+        return LCS(s, rev, s.length(), rev.length());
     }
 }
