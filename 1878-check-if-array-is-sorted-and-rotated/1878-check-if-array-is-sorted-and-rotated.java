bd9729c6d1966e0c697919c663f6d4bdd78cf639
Anshul Yadav <156102877+Anshu10101@users.noreply.github.com>
2025-11-16 00:12:53 +0530

    1878-CHECK-IF-ARRAY-IS-SORTED-AND-ROTATED Stats: Time: 0 ms (100%), Space: 42.8 MB (14.61%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.java
@@ -3,7 +3,7 @@ class Solution {
         // here we compare all the neighbouring elemnts and check whether they are in somewhat sorted
         // there will be a small change due to rotation in the array at only one place.
         // so if there are irregularities more than once, return false
-        // else return true;
+        // else return true
         int irregularities = 0;
         int length = nums.length;
         for (int i=0; i<length; i++) {
